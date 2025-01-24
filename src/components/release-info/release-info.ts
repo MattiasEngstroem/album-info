ea1817c4e59eced4cc5ca20c294bf41476ca2622
MattiasEngstroem <jazzmatte@hotmail.com>
2025-01-24 11:45:18 +0100

    gjort klart all albuminfo

--- src/components/release-info/release-info.ts
+++ src/components/release-info/release-info.ts
@@ -11,8 +11,44 @@ export const releaseInfo = async (releaseID: string): Promise<void> => {
   console.log(release);
 
   const imageElement = document.createElement("img");
+  imageElement.classList.add("album-cover");
   imageElement.src = release.images[0].uri;
   imageElement.alt = "Skivomslag";
   //imageElement.width = 300;
   releaseInfoDiv.appendChild(imageElement);
+
+  const albumName = document.createElement("h3");
+  albumName.classList.add("album-name");
+  albumName.textContent = `${release.artists_sort}: ${release.title} (${release.released})`;
+  releaseInfoDiv.appendChild(albumName);
+
+  release.tracklist.forEach((t) => {
+    const trackName = document.createElement("p");
+    trackName.classList.add("track-name");
+    trackName.innerHTML = `<br>${t.position} ${t.title} ${t.duration}`;
+    releaseInfoDiv.appendChild(trackName);
+
+    if (t.extraartists) {
+      t.extraartists.forEach((e) => {
+        const extraArtistOnTrack = document.createElement("p");
+        extraArtistOnTrack.classList.add("extra-artist-on-track");
+        extraArtistOnTrack.textContent = `${e.role}: ${e.name}`;
+        releaseInfoDiv.appendChild(extraArtistOnTrack);
+      });
+    }
+  });
+
+  const personnel = document.createElement("h3");
+  personnel.classList.add("personnel-heading");
+  personnel.innerHTML = "<br>Personnel:";
+  releaseInfoDiv.appendChild(personnel);
+
+  release.extraartists.forEach((e) => {
+    const extraArtist = document.createElement("p");
+    extraArtist.classList.add("personnel-member");
+    extraArtist.textContent = `${e.role}: ${e.name}${
+      e.tracks ? " (" + e.tracks + ")" : ""
+    }`;
+    releaseInfoDiv.appendChild(extraArtist);
+  });
 };
